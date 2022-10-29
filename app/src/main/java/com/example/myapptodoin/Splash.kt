9db7028e2b3257587d2195afc657e218ee5f7bde
Claudia <cmflorezg14@gmail.com>
2022-10-28 22:01:22 -0500

    Actualizando el proyecto TODOIN

--- app/src/main/java/com/example/myapptodoin/Splash.kt
+++ app/src/main/java/com/example/myapptodoin/Splash.kt
@@ -1,11 +1,28 @@
 package com.example.myapptodoin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.myapptodoin.databinding.ActivitySplashBinding
+import kotlinx.coroutines.delay
 
 class Splash : AppCompatActivity() {
+    private lateinit var binding:ActivitySplashBinding
+    private lateinit var handler:Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding=ActivitySplashBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+        handler= Handler(Looper.myLooper()!!)
+        handler.postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+        }, 4000)
     }
 }
+
+
+
+
