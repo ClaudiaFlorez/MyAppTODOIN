5418e3ea7167740f297516b7b78ea8540afd0ba0
cyndyrella <cindyargote006@gmail.com>
2022-10-31 18:33:23 -0500

    cambios en Villavo

--- app/src/main/java/com/example/myapptodoin/CampingVillavo.kt
+++ app/src/main/java/com/example/myapptodoin/CampingVillavo.kt
@@ -1,11 +1,20 @@
 package com.example.myapptodoin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapptodoin.databinding.ActivityCaminatasvillavoBinding
+import com.example.myapptodoin.databinding.ActivityCampingvillavoBinding
 
 class CampingVillavo : AppCompatActivity() {
+    private lateinit var binding: ActivityCampingvillavoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_campingvillavo)
+        binding = ActivityCampingvillavoBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.villavo3.setOnClickListener {
+            startActivity(Intent(this,TodoInVillavo::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/myapptodoin/VillavoRegistro.kt
+++ app/src/main/java/com/example/myapptodoin/VillavoRegistro.kt
@@ -1,11 +1,24 @@
 package com.example.myapptodoin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapptodoin.databinding.ActivityBogotaregistroBinding
+import com.example.myapptodoin.databinding.ActivityVillavoregistroBinding
 
 class VillavoRegistro : AppCompatActivity() {
+    private lateinit var binding: ActivityVillavoregistroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_villavoregistro)
+        binding = ActivityVillavoregistroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.planeaydisfrutaaqui.setOnClickListener {
+            startActivity(Intent(this, TodoinExplorarRutas::class.java))
+        }
+        binding.inicio2.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
