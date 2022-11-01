193dc07c72c807c31ba0272531575969c284566c
Claudia <cmflorezg14@gmail.com>
2022-10-31 19:21:46 -0500

    Actualizando el proyecto TODOIN

--- app/src/main/java/com/example/myapptodoin/CampingBogota.kt
+++ app/src/main/java/com/example/myapptodoin/CampingBogota.kt
@@ -1,11 +1,21 @@
 package com.example.myapptodoin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapptodoin.databinding.ActivityBogotaInfo2Binding
+import com.example.myapptodoin.databinding.ActivityCampingbogotaBinding
 
 class CampingBogota : AppCompatActivity() {
+    private lateinit var binding: ActivityCampingbogotaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_campingbogota)
+        binding = ActivityCampingbogotaBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnbogota7.setOnClickListener {
+            startActivity(Intent(this, TodoInBogota::class.java))
+        }
     }
 }
+
--- app/src/main/java/com/example/myapptodoin/ToursBogota.kt
+++ app/src/main/java/com/example/myapptodoin/ToursBogota.kt
@@ -1,11 +1,26 @@
 package com.example.myapptodoin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapptodoin.databinding.ActivityBogotaInfo2Binding
+import com.example.myapptodoin.databinding.ActivityToursbogotaBinding
 
 class ToursBogota : AppCompatActivity() {
+    private lateinit var binding: ActivityToursbogotaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_toursbogota)
+        binding = ActivityToursbogotaBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.bogot6.setOnClickListener {
+            startActivity(Intent(this, TodoInBogota::class.java))
     }
 }
+
+    }
+    /*binding.btnplanesbogota.setOnClickListener {
+        startActivity(Intent(this, TodoInBogota::class.java))
+    }
+    binding.btndeatras6.setOnClickListener {
+        startActivity(Intent(this, BogotaInfo1::class.java))*/
